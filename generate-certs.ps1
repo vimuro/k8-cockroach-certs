31de46ef0ea73d4e324d26cbb0d53f0d88932272
Michael Braun <michael@web4brauns.de>
2021-11-07 20:49:01 +0100

    Add Read-Host to PowerShell script

--- generate-certs.ps1
+++ generate-certs.ps1
@@ -1,9 +1,14 @@
+$CLUSTER_NAME = Read-Host "Name of the cockroachdb-cluster: "
+$CLUSTER_NAMESPACE = Read-Host "Namespace of the cockroachdb-cluster (e.g. default): "
+$CLUSTER_DOMAIN = Read-Host "Domain of the kubernetes-cluster (e.g. local): "
+$CLUSTER_USERS = Read-Host "Users for which the certificates should be generated (all users different from root, e.g. admin): "
+
 docker pull ghcr.io/vimuro/k8-cockroach-certs:master
 docker run -v $HOME/.kube/config:/root/.kube/config:ro `
-    -e CLUSTER_NAME="uac-database" `
-    -e CLUSTER_NAMESPACE="vis-uac-backend" `
-    -e CLUSTER_DOMAIN="local" `
-    -e CLUSTER_USERS="" `
+    -e CLUSTER_NAME="$CLUSTER_NAME" `
+    -e CLUSTER_NAMESPACE="$CLUSTER_NAMESPACE" `
+    -e CLUSTER_DOMAIN="$CLUSTER_DOMAIN" `
+    -e CLUSTER_USERS="$CLUSTER_USERS" `
     ghcr.io/vimuro/k8-cockroach-certs:master
 
 # docker run -v $HOME/.kube/config:/root/.kube/config:ro k8-cockroach ./create-certs.sh
